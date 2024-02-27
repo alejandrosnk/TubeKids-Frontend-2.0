4cf3dff3d0ef4add79ead4536d79e4e1ba059e1f
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-02-27 15:43:23 -0600

    Creación de componentes y rutas
    Se crearon los componentes y rutas necesarias al momento inicial del proyecto, se cree que se añadirán más.

--- tubekids_frontend/src/App.js
+++ tubekids_frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from 'react-router-dom'
+import Login from './Components/Login/login'
+import Home from './Components/Home/home'
+import Watch from './Components/Watch/watch'
+import AdministrationKids from './Components/Administration/AdministrationKids/AdministrationKids'
+import AdministrationPlaylist from './Components/Administration/AdministrationPlaylist/AdministrationPlaylist'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/watch" element={<Watch/>}/>
+          <Route path="/adminKids" element={<AdministrationKids/>}/>
+          <Route path="/adminPlaylist" element={<AdministrationPlaylist/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ tubekids_frontend/src/Components/Administration/AdministrationPlaylist/AdministrationPlaylist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdministrationPlaylist = () => {
+  return (
+    <div>AdministrationPlaylist</div>
+  )
+}
+
+export default AdministrationPlaylist
